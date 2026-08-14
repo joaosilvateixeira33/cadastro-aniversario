36fea3d40da26423c1059c24e0a28f8792b40495
Joao Marcos Silva <joaoteixeirasilva33@gmail.com>
2026-08-14 12:27:47 -0300

    feat: day 2 completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 let form = document.querySelector('.js-form')
 let field = document.querySelectorAll('.js-field')
 
+function formatToBR(isoDate) {
+  if (!isoDate) return ''
+  const [year, month, day] = isoDate.split('-')
+  return `${day}/${month}/${year}`
+}
+
 form.addEventListener('submit', function sendData(event){
     event.preventDefault()
 
     let name = field[0].value
-    let birthDay = field[1].value
+    let birthDay = formatToBR(field[1].value)
 
     console.log(name)
     console.log(birthDay);
