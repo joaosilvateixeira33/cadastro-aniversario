314e1fa938be85af6e17f7254ef6862aa18bec09
Joao Marcos Silva <joaoteixeirasilva33@gmail.com>
2026-08-17 12:37:39 -0300

    feat: adding action edit person data

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,52 @@ let btnSend = document.querySelector('.js-btn-send')
 let fieldName = document.querySelector('.js-field-name')
 let fieldBirthday = document.querySelector('.js-field-birthday')
 
+let editingIndex = null
+
 function formatToBR(isoDate) {
   if (!isoDate) return ''
   const [year, month, day] = isoDate.split('-')
   return `${day}/${month}/${year}`
 }
 
+function formatToUSA(isoDate){
+  if (!isoDate) return ''
+  const [day, month, year] = isoDate.split('/')
+  return `${year}-${month}-${day}`
+}
+
 function renderTable() {
   const persons = JSON.parse(localStorage.getItem('persons')) || []
   const tableBody = document.querySelector('.js-table')
 
+  
   if (!tableBody) return
-
+  
   tableBody.innerHTML = ''
-
-  persons.forEach(person => {
+  
+  persons.forEach((person, index) => {
     const row = document.createElement('tr')
+    const btnEdit = document.createElement('button')
+    const actions = document.createElement('td')
+
+    btnEdit.textContent = 'Editar'
+    btnEdit.classList.add('btn-edit')
+
+    btnEdit.addEventListener('click', function () {
+      const person = persons[index]
+      fieldName.value = person.name
+      fieldBirthday.value = formatToUSA(person.birthDay)
+      editingIndex = index
+    })
+
     row.innerHTML = `
       <td>${person.name}</td>
       <td>${person.birthDay}</td>
     `
+    actions.appendChild(btnEdit)
+    row.appendChild(actions)
     tableBody.appendChild(row)
+
   })
 }
 
@@ -36,7 +61,18 @@ form.addEventListener('submit', function sendData(event) {
   if (!name.trim() || !birthDay) return
 
   let persons = JSON.parse(localStorage.getItem('persons')) || []
-  persons.push({ name, birthDay })
+
+  if (editingIndex !== null) {
+    persons[editingIndex] = {
+      name,
+      birthDay
+    }
+
+    editingIndex = null
+  } else {
+    persons.push({ name, birthDay })
+  }
+  
   localStorage.setItem('persons', JSON.stringify(persons))
 
   form.reset()
